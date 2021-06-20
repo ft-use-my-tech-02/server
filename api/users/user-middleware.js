9dad14e7d0fb5c84ad0dc8f208e6da0a235b4fcb
blackenjoy27 <lixunxin@gmail.com>
2021-06-19 22:52:32 -0400

    wip

---
+++ api/users/user-middleware.js
@@ -0,0 +1,29 @@
+const {JWT_SECRECT} = require("../config/secrect");
+const jwt = require("jsonwebtoken");
+
+const restrict = (req,res,next)=>{
+    const token = req.headers.authorization;
+    if(token){
+        jwt.verify(token,JWT_SECRECT,(error, decoded)=>{
+            if(error){  
+                next({
+                    status:401,
+                    message:"invalid credential"
+                })
+            }else{
+                req.decodedJwt = decoded;
+                next();
+            }
+        })
+    }else{
+        next({
+            message:"token is required"
+        })
+    }
+}
+
+module.exports = {
+    restrict,
+    
+}
+

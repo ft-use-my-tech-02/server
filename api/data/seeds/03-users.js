56d3cfb6093a366184b1565cb38bac75543543b4
blackenjoy27 <lixunxin@gmail.com>
2021-09-12 13:13:17 -0400

    update server

--- api/data/seeds/03-users.js
+++ api/data/seeds/03-users.js
@@ -1,8 +1,8 @@
 exports.seed = function (knex, Promise) {
   return knex('users').insert([
-    { username: "adam1",password:"123",email:"abc123@gmail.com",role_id: 1 },
-    { username: "adam2",password:"123",email:"abcd123@gmail.com",role_id: 2 },
-    { username: "owner",password:"123",email:"b123@gmail.com",role_id: 1 },
-    { username: "renter",password:"123",email:"a123@gmail.com",role_id: 2 },
+    { name: "Kyle", username: "adam1", password: "123", email: "abc123@gmail.com", role_id: 1 },
+    { name: "July", username: "adam2", password: "123", email: "abcd123@gmail.com", role_id: 2 },
+    { name: "June", username: "owner", password: "123", email: "b123@gmail.com", role_id: 1 },
+    { name: "Anna", username: "renter", password: "123", email: "a123@gmail.com", role_id: 2 },
   ]);
 };

b8d854b89852b02a589a5a20c56c514fd0d24420
blackenjoy27 <lixunxin@gmail.com>
2021-09-23 17:19:52 -0400

    applied nodemailer to the user router

--- api/users/user-router.js
+++ api/users/user-router.js
@@ -1,24 +1,57 @@
 const router = require("express").Router();
+const nodemailer = require("nodemailer");
+
 const {
     restrict,
     validateChangePassword
 } = require("./user-middleware");
 const users = require("./user-model");
 
-router.put("/:id", restrict, validateChangePassword, (req,res,next)=>{
-    users.updateUser(req.params.id, req.body)
-    .then(([user])=>{
-        res.status(201).json(user)
-    })
-    .catch(next);
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.TRANSPORTER,
+        pass: process.env.PASS,
+    },
 })
 
-router.get("/:id",restrict, (req,res,next)=>{
+router.put("/:id", restrict, validateChangePassword, (req, res, next) => {
+    users.updateUser(req.params.id, req.body)
+        .then(([user]) => {
+            res.status(201).json(user)
+        })
+        .catch(next);
+})
+
+router.get("/:id", restrict, (req, res, next) => {
     users.findById(req.params.id)
-    .then(user=>{
-        res.status(200).json(user)
-    })
-    .catch(next);
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(next);
+})
+
+router.post("/", (req, res, next) => {
+    const { feedback } = req.body;
+
+    const msg = {
+        from: process.env.TRANSPORTER,
+        to: process.env.RECEIVER,
+        subject: "New Feedback For Use My Tech",
+        text: `${feedback}`
+    }
+
+    transporter.sendMail(msg, (error) => {
+        if (error) {
+            console.log(typeof process.env.TRANSPORTER);
+            console.log(process.env.PASS);
+            res.json(error);
+        } else {
+            res.json("Thanks for your feedback!");
+        }
+    });
 })
 
 module.exports = router;
